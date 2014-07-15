fc6c103a67f0bdda28aa311cf301732b7beaa5ef
drice0331 <drice@Davids-MacBook-Air.local>
2014-07-14 21:58:41 -0700

    visits class

---
+++ src/com/dtf/hellobeacon/Visit.java
@@ -0,0 +1,83 @@
+package com.dtf.hellobeacon;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import android.text.format.DateFormat;
+
+public class Visit {
+	
+	protected Date enterTime;
+	protected Date exitTime;
+	protected long lengthOfStay;
+	
+	public Visit()
+	{
+		setTime(enterTime);
+		lengthOfStay = 0;
+	}
+	
+	/**
+	 * sets the given date (time) to the current date in the desired format
+	 * @param time
+	 */
+	public void setTime(Date time){
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm a");
+		time = new Date();
+		GregorianCalendar timeCal = new GregorianCalendar();
+		String timeString = dateFormat.format(timeCal.getTime());
+		try {
+			time = dateFormat.parse(timeString);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setLengthOfStay() {
+		setTime(exitTime);
+		long diff = enterTime.getTime() - exitTime.getTime();
+		
+		long diffHours = diff / (60 * 60 * 1000) % 24;
+		long diffMinutes = diff / (60 * 1000) % 60;
+		this.lengthOfStay = diffHours + (diffMinutes)/60;
+		
+	}
+	
+	//GETTERS
+	
+	public Date getEnterDate() {
+		
+		if(enterTime != null)
+		{
+			return enterTime;
+		}
+		return null;
+		
+		/*
+		try {
+			return enterTime;
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+		*/
+	}
+	
+	public Date getExitDate() {
+		if(exitTime != null)
+		{
+			return exitTime;
+		}
+		return null;
+	}
+	
+	public long getLengthOfStay() {
+		return lengthOfStay;
+	}
+	
+
+	
+	
+}
