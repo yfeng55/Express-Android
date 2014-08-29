f23c2bbc70443a5c874e2dd05fe8d1feb1f12a81
Yijie Feng <yijiefeng@gmail.com>
2014-08-29 00:30:51 -0700

    asdf

--- src/com/dtf/hellobeacon/MyStatsActivity.java
+++ src/com/dtf/hellobeacon/MyStatsActivity.java
@@ -1,10 +1,19 @@
 package com.dtf.hellobeacon;
-import com.example.hellobeacon.R;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.example.hellobeacon.R;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.GenericTypeIndicator;
+import com.firebase.client.ValueEventListener;
 
 
 public class MyStatsActivity extends Activity{
@@ -13,6 +22,7 @@ public class MyStatsActivity extends Activity{
 	private String firstname;
 	private String lastname;
 	private TextView myname;
+	List<Long> visits;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +33,44 @@ public class MyStatsActivity extends Activity{
 		prefs = this.getSharedPreferences("com.dtf.hellobeacon", 0);
 		firstname = prefs.getString("firstName", "No First Name");
 		lastname = prefs.getString("lastName", "No Last Name");
+		
 		myname = (TextView) findViewById(R.id.tv_MyName);
 		myname.setText(firstname + " " + lastname);
 		
+		visits = new ArrayList<Long>();
+		
+		
+		//get the user's visits from firebase
+		
+		Firebase visitsref = new Firebase("https://hellobeacon.firebaseio.com/Users/" + firstname + lastname + "/Visits");
+		
+		visitsref.addValueEventListener(new ValueEventListener() {
+			
+			@Override
+			public void onDataChange(DataSnapshot snapshot) {
+				//GenericTypeIndicator<List<String>> t = new GenericTypeIndicator<List<String>>() {};
+			    //List<String> messages = snapshot.getValue(t);
+			}
+
+			@Override
+			public void onCancelled(FirebaseError error) {
+				System.err.println("Listener was cancelled");
+			}
+
+		});
+		
+		
+		//display all of the user's visits on the screen
+//		for(long v : visits){
+//			Log.i("EEE", Long.toString(v));
+//		}
+//		
 		
 	}
 	
 }
+
+
+
+
+
