6a2992d4d44dca889bbba091c1b78125e31366c9
Yijie Feng <yijiefeng@gmail.com>
2014-09-30 23:16:56 -0700

    asdf

---
+++ src/com/dtf/hellobeacon/HelpActivity.java
@@ -0,0 +1,14 @@
+package com.dtf.hellobeacon;
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.hellobeacon.R;
+
+
+public class HelpActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_class_schedule);
+	}
+}
